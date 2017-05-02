3f9249c6e3acccbe5be77c5d38fe563b55283891
jlon14 <john.long@nike.com>
2017-05-01 23:10:00 -0700

    Make war not jar

--- msr/src/main/java/com/aessense/agm/MsrApplication.java
+++ msr/src/main/java/com/aessense/agm/MsrApplication.java
@@ -3,6 +3,8 @@ package com.aessense.agm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Main application class
@@ -10,7 +12,15 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  *
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class MsrApplication {
+public class MsrApplication extends SpringBootServletInitializer {
+
+	/**
+	 * Required to make war instead of jar.
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MsrApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsrApplication.class, args);
--- sensor-history/src/main/java/com/aessense/agm/sensorhistory/Application.java
+++ sensor-history/src/main/java/com/aessense/agm/sensorhistory/Application.java
@@ -5,6 +5,8 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,9 +17,17 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+	/**
+	 * Required to make war instead of jar.
+	 */
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    	return builder.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
     	log.info("Setting default timezone to UTC");
     	TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(Application.class, args);
